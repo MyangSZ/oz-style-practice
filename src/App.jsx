92f3a4a5efe78de54914dc1c3530af8e45e0eee6
MyangSZ <unoyou@naver.com>
2024-11-23 19:36:29 +0900

    [React] 11일차 과제 init / Styled-Component로 바꾸기

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
-import "./App.scss";
+import styled from "styled-components";
+import "./styled/styled";
 import { contents } from "./assets/data/data";
 import Content from "./components/Content";
 import Header from "./components/Header";
+import { flexMixin  } from "./styled/styled";
+
+
+
+const ContentContainer = styled.section`
+  ${flexMixin({justify: "center", align: "center", wrap: "wrap", gap: "20px"})}
+  padding: 20px 40px;
+`;
 
 function App() {
   return (
-    <main>
+    <>
       <Header />
-      <section>
+      <ContentContainer>
         {contents.map((el) => (
           <Content key={el.id} content={el} />
         ))}
-      </section>
-    </main>
+      </ContentContainer>
+    </>
   );
 }
 
